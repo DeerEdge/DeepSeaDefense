c158de17a7f3c42835279887cf6d578bd9a6d18b
DeerEdge <vislawath.21@gmail.com>
2024-11-19 02:20:27 -0500

    button class additions

--- check_functions.py
+++ check_functions.py
@@ -5,6 +5,7 @@ def isWithinRect(centerX, centerY, width, height, mouseX, mouseY):
         return True
     return False
 
+# Checks if a mouse action occurred inside a rectangle (rects aligned by their top left)
 def isWithinRectTopLeft(topLeftX, topLeftY, width, height, mouseX, mouseY):
     if (topLeftX <= mouseX <= topLeftX + width) and (topLeftY - height <= mouseY <= topLeftY + height):
         return True
--- widgets.py
+++ widgets.py
@@ -1,5 +1,22 @@
-
 class Button:
-    def __init__(self, name):
+    def __init__(self, name, centerX, centerY, width, height, label='', fill='white', border=None, borderWidth=2,
+                 align='center', textFill='black', textFont='monospace'):
         self.id = name
+        self.centerX = centerX
+        self.centerY = centerY
+        self.width = width
+        self.height = height
+        self.label = label
+        self.fill = fill
+        self.border = border
+        self.borderWidth = borderWidth
+        self.align = align
+        self.textFill = textFill
+        self.textFont = textFont
         self.actions = []
+
+    def getShape(self):
+        return [self.centerX, self.centerY, self.width, self.height, self.fill, self.border, self.borderWidth, self.align]
+
+    def getLabel(self):
+        return(self.label, self.centerX, self.centerY, self.textFill, self.textFont)
